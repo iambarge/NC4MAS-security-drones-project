d88648c211b52c0abbac39841e043321e1608a6d
Nicolò Bargellesi <nicobkt8@gmail.com>
2022-01-11 11:24:36 +0100

    License update

--- BLP.m
+++ BLP.m
@@ -1,4 +1,4 @@
-% Copyright (c) 2019, Nicolò Bargellesi, Luca Facin & Lorenzo Marchini
+% Copyright (c) 2020, Nicolò Bargellesi, Luca Facin & Lorenzo Marchini
 %
 % This source code is licensed under the MIT-style license found in the
 % LICENSE file in the root directory of this source tree.
