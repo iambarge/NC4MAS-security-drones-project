fc6243f3df08c906c9c60ed9f28d2f9ce51a54b2
Nicolò Bargellesi <nicobkt8@gmail.com>
2022-01-11 11:28:14 +0100

    License update

--- envEdges.m
+++ envEdges.m
@@ -1,3 +1,9 @@
+% Copyright (c) 2020, Nicolò Bargellesi, Luca Facin & Lorenzo Marchini
+%
+% This source code is licensed under the MIT-style license found in the
+% LICENSE file in the root directory of this source tree.
+%
+
 function [Em,Am,A] = envEdges(V,Vm,Vmap,res)
 %ENVEDGES Build edges of the graph
 
