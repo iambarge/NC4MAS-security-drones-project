a3c8b97970dcf34fa1c5ad44d5c1053c30cf1cb3
Nicolò Bargellesi <nicobkt8@gmail.com>
2022-01-11 11:21:55 +0100

    License update

--- BLP.m
+++ BLP.m
@@ -1,3 +1,9 @@
+% Copyright (c) 2019, Nicolò Bargellesi, Luca Facin & Lorenzo Marchini
+%
+% This source code is licensed under the MIT-style license found in the
+% LICENSE file in the root directory of this source tree.
+%
+
 %% Parameters
 load('Data/C.mat')      % load desired environment
 env = C;                % rename selected environment
