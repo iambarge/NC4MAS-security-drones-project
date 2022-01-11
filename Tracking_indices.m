5175793cea6face7e4b2bd6637ccad9116964c5d
Nicolò Bargellesi <nicobkt8@gmail.com>
2022-01-11 11:27:13 +0100

    License update

--- Tracking_indices.m
+++ Tracking_indices.m
@@ -1,3 +1,9 @@
+% Copyright (c) 2020, Nicolò Bargellesi, Luca Facin & Lorenzo Marchini
+%
+% This source code is licensed under the MIT-style license found in the
+% LICENSE file in the root directory of this source tree.
+%
+
 clear
 close all
 clc
