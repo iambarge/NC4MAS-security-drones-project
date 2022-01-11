2146df56a79a9dc24e070d43e0ea41a64d8c053d
Nicolò Bargellesi <nicobkt8@gmail.com>
2022-01-11 11:26:24 +0100

    License update

--- Simulation_preprocessing.m
+++ Simulation_preprocessing.m
@@ -1,3 +1,9 @@
+% Copyright (c) 2020, Nicolò Bargellesi, Luca Facin & Lorenzo Marchini
+%
+% This source code is licensed under the MIT-style license found in the
+% LICENSE file in the root directory of this source tree.
+%
+
 %% Define Environment
 % Borders
 B1 = [0 0; 0 9; 7 9; 7 6; 3 6; 3 3; 7 3; 7 0];      % C Environment
